8273c4e54436fb5ad8c6fa85abdf6f37fb026246
dale <18717394542@163.com>
2014-12-03 01:31:59 +0800

    Add 12.2_main.js

--- Pos/pos_v1/src/main.js
+++ Pos/pos_v1/src/main.js
@@ -1,17 +1,18 @@
 //TODO: Please write code in this file.
 function printInventory(inputs){
-  
-  var save = 0;
-  var myArray = [];
-  var expectText = '***<没钱赚商店>购物清单***\n';
+  var numInfo = infoNum(inputs);
+  var myArray = addProperty(numInfo);
+  setString(myArray);
+}
 
+// 将商品的barcode、num信息汇总
+function infoNum(inputs){
+  var myArray = [];                                  // 我的数据库数组（存放对象）
   for(var i = 0; i < inputs.length; i++){
     var b = true;
     if(inputs[i].split("-").length > 1){
       var arr = inputs[i].split("-");
-      //console.log(arr);
-      myArray[myArray.length] = { barcode : arr[0], num : arr[1]};
-      b = false;
+      myArray[myArray.length] = { barcode : arr[0] , num : arr[1] };
       continue;
     }
     for(var j = 0; j < myArray.length; j++){
@@ -21,22 +22,34 @@ function printInventory(inputs){
       }
     }
     if(b){
-      myArray[myArray.length] = { barcode : inputs[i], num : 1};
+      myArray[myArray.length] = { barcode : inputs[i] , num : 1 };
     }
   }
+  return myArray;
+}
 
-
-for(var i = 0; i < myArray.length; i++){
+// 给myArray加入price、name、unit属性
+function addProperty(myArray){
+  for(var i = 0; i < myArray.length; i++){
     for(var j = 0; j < loadAllItems().length; j++){
       if(myArray[i].barcode === loadAllItems()[j].barcode){
         myArray[i].price = loadAllItems()[j].price;
         myArray[i].name = loadAllItems()[j].name;
         myArray[i].unit = loadAllItems()[j].unit;
+        break;
       }
     }
   }
+  return myArray;
+}
 
-  var tt = '';
+// 计算小计并向expectText中添加字符串
+function setString(myArray){
+  var save = 0;                                      // 节省钱数
+  var expectText = '***<没钱赚商店>购物清单***\n';      // 打印输出字符串
+  var freeInfo = '';                                 // 赠送商品信息
+  var allSum = 0;                                    // 总计钱数
+
   for(var i = 0; i < myArray.length; i++){
     var b = true;
     for(var j = 0; j < loadPromotions()[0].barcodes.length; j++){
@@ -44,40 +57,18 @@ for(var i = 0; i < myArray.length; i++){
         b = false;
         myArray[i].sum = myArray[i].price * myArray[i].num - (Math.floor((myArray[i].num)/3)) * myArray[i].price;
         save += (Math.floor((myArray[i].num)/3)) * myArray[i].price;
-        tt += '名称：'+ myArray[i].name +'，数量：'+ Math.floor((myArray[i].num)/3) + myArray[i].unit+'\n';
+        freeInfo += '名称：'+ myArray[i].name +'，数量：'+ Math.floor((myArray[i].num)/3) + myArray[i].unit + '\n';
+        break;
       }
     }
     if(b){
       myArray[i].sum = myArray[i].price * myArray[i].num;
     }
-  }
-
-  var expectText =
-  '***<没钱赚商店>购物清单***\n' ;
-  var allsum = 0;
-  for(var i = 0; i < myArray.length; i++){
     expectText += '名称：' + myArray[i].name + '，' + '数量：'+ myArray[i].num + myArray[i].unit + '，单价：' + myArray[i].price.toFixed(2) +'(元)，小计：' + myArray[i].sum.toFixed(2) +'(元)\n';
-    allsum += myArray[i].sum;
+    allSum += myArray[i].sum;
   }
 
-  expectText += '----------------------\n' +
-  '挥泪赠送商品：\n' ;
-  expectText += tt;
-  expectText += '----------------------\n';
-  expectText += '总计：'+ allsum.toFixed(2) +'(元)\n' + '节省：'+ save.toFixed(2) +'(元)\n';
-  expectText +=  '**********************';
+  expectText += '----------------------\n' + '挥泪赠送商品：\n' + freeInfo + '----------------------\n' + '总计：'+ allSum.toFixed(2) +'(元)\n' + '节省：'+ save.toFixed(2) +'(元)\n' + '**********************';
 
-  /*'名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
-  //  '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
-  '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
-  '----------------------\n' +
-  '挥泪赠送商品：\n' +
-  '名称：雪碧，数量：1瓶\n' +
-  '名称：方便面，数量：1袋\n' +
-  '----------------------\n' +
-  '总计：51.00(元)\n' +
-  '节省：7.50(元)\n' +
-  '**********************';*/
   console.log(expectText);
-
 }
