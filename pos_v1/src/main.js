4471339d2e75930887d1383833af23e95fa3ef51
dale <18717394542@163.com>
2014-12-03 09:39:20 +0800

    add empty line to improve readble

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -6,29 +6,38 @@ function printInventory(inputs){
 
 function infoNum(inputs){
   var myArray = [];
+
   for(var i = 0; i < inputs.length; i++){
+
     var b = true;
+
     if(inputs[i].split("-").length > 1){
       var arr = inputs[i].split("-");
       myArray[myArray.length] = { barcode : arr[0] , num : arr[1] };
       continue;
     }
+
     for(var j = 0; j < myArray.length; j++){
       if(inputs[i] === myArray[j].barcode){
         myArray[j].num++;
         b = false;
       }
     }
+
     if(b){
       myArray[myArray.length] = { barcode : inputs[i] , num : 1 };
     }
   }
+
   return myArray;
 }
 
 function addProperty(myArray){
+
   for(var i = 0; i < myArray.length; i++){
+
     for(var j = 0; j < loadAllItems().length; j++){
+
       if(myArray[i].barcode === loadAllItems()[j].barcode){
         myArray[i].price = loadAllItems()[j].price;
         myArray[i].name = loadAllItems()[j].name;
@@ -37,6 +46,7 @@ function addProperty(myArray){
       }
     }
   }
+
   return myArray;
 }
 
@@ -47,7 +57,9 @@ function setString(myArray){
   var allSum = 0;
 
   for(var i = 0; i < myArray.length; i++){
+
     var b = true;
+
     for(var j = 0; j < loadPromotions()[0].barcodes.length; j++){
       if(myArray[i].barcode === loadPromotions()[0].barcodes[j]){
         b = false;
@@ -57,10 +69,13 @@ function setString(myArray){
         break;
       }
     }
+
     if(b){
       myArray[i].sum = myArray[i].price * myArray[i].num;
     }
+
     expectText += '名称：' + myArray[i].name + '，' + '数量：'+ myArray[i].num + myArray[i].unit + '，单价：' + myArray[i].price.toFixed(2) +'(元)，小计：' + myArray[i].sum.toFixed(2) +'(元)\n';
+
     allSum += myArray[i].sum;
   }
 
