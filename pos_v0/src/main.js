18a82d2758617690471cac3cdbf524f85ca90ac0
dale <18717394542@163.com>
2014-12-08 00:07:31 +0800

    use cartItem instead of inputs[i]

--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -6,13 +6,14 @@ function printInventory(inputs) {
 function getInventoryText(inputs) {
   var cartItemsText = '***<没钱赚商店>购物清单***\n';
   var totalAccount = 0;
+  var cartItem = inputs[i];
 
   for(var i = 0; i < inputs.length; i++) {
-    cartItemsText += '名称：' + inputs[i].name + '，数量：' + inputs[i].count +
-                     inputs[i].unit + '，单价：' + inputs[i].price.toFixed(2) +
-                     '(元)，小计：' +
-                     (inputs[i].count * inputs[i].price).toFixed(2) + '(元)\n';
-    totalAccount += inputs[i].count * inputs[i].price;
+    cartItemsText += '名称：' + cartItem.name + '，数量：' + cartItem.count +
+                     cartItem.unit + '，单价：' +
+                     cartItem.price.toFixed(2) +'(元)，小计：' +
+                     (cartItem[i].count * cartItem.price).toFixed(2) + '(元)\n';
+    totalAccount += cartItem[i].count * cartItem[i].price;
   }
   var summaryText = getSummaryText(totalAccount);
   var inventoryText = joinText(cartItemsText, '----------------------\n',
