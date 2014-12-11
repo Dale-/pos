822127f665a0f8786451ca19f19a54c08d782ba1
dale <18717394542@163.com>
2014-12-11 10:11:36 +0800

    recover //console.log(content);

--- pos_v2_3rdlib/src/main.js
+++ pos_v2_3rdlib/src/main.js
@@ -2,5 +2,5 @@ function printInventory(tags) {
   var cartItems = new CartItems();
   var pos = new Pos(cartItems.getCartItems(tags));
   var content = pos.joinText();
-  //console.log(content);
+  console.log(content);
 }
