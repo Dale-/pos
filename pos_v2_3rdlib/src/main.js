8ed2b2aba559944bc4afd7d6812714a7ebf3fcde
dale <18717394542@163.com>
2014-12-11 22:25:57 +0800

    simplify Inventory.prototype.joinText() method

--- pos_v2_3rdlib/src/main.js
+++ pos_v2_3rdlib/src/main.js
@@ -2,13 +2,9 @@ function printInventory(tags) {
   var scanner = new Scanner();
   var cart = new Cart();
   _.forEach(tags, function(tag) {
-    var cartItem = scanner.scan(tag);
-    cart.addCartItem(cartItem);
+    cart.addCartItem(scanner.scan(tag));
   });
-  cart.setPayCount();
-  console.log(cart.cartItems);
-  // var cart = new Cart();
-  var pos = new Pos(cart.cartItems);
-  var content = pos.joinText();
-  console.log(content);
+  
+  var inventory = new Inventory(cart.cartItems);
+  console.log(inventory.inventoryText);
 }
--- pos_v2_3rdlib/src/model/cart.js
+++ pos_v2_3rdlib/src/model/cart.js
@@ -2,46 +2,6 @@ function Cart() {
    this.cartItems = [];
 }
 
-// Cart.pushCartItem = function(barcode , count, cartItems) {
-//   var newItem = _.find(loadAllItems(), { barcode: barcode});
-//   var isPromotion = Promotion.isPromotionBarcode(barcode);
-//   cartItems.push(new CartItem( newItem , count ,isPromotion));
-// };
-//
-// Cart.getCartItemsPayCount = function(cartItems) {
-//   _.forEach(cartItems,function(cartItem){
-//     var payCount = cartItem.count;
-//     if(cartItem.isPromotion){
-//       payCount = payCount - Math.floor(payCount/3) ;
-//     }
-//     cartItem.payCount = payCount;
-//   });
-// };
-//
-// Cart.prototype.getCartItems = function(tags) {
-//   var cartItems = this.cartItems;
-//   _.forEach(tags, function (tag) {
-//     var splitedArray = tag.split('-');
-//     barcode = splitedArray[0];
-//     count = 1;
-//     if(splitedArray[1]) {
-//       count = parseFloat(splitedArray[1]);
-//     }
-//
-//     var cartItem = _.find(cartItems, function(cartItem) {
-//       return barcode === cartItem.item.barcode;
-//     });
-//     if(cartItem) {
-//       cartItem.count += count ;
-//     }else {
-//       Cart.pushCartItem(barcode, count, cartItems);
-//     }
-//   });
-//
-//   Cart.getCartItemsPayCount(cartItems);
-//   return cartItems;
-// };
-
 Cart.prototype.addCartItem = function(cart_item) {
   var cartItems = this.cartItems;
   var newCartItem = _.find(cartItems, function(cartItem) {
@@ -54,14 +14,4 @@ Cart.prototype.addCartItem = function(cart_item) {
       Promotion.isPromotionBarcode(cart_item.item.barcode);
     cartItems.push(cart_item);
   }
-};
-
-Cart.prototype.setPayCount = function() {
-  var cartItems = this.cartItems;
-  _.forEach(cartItems, function(cartItem){
-    cartItem.payCount = cartItem.count;
-    if(cartItem.isPromotion) {
-      cartItem.payCount = cartItem.payCount - Math.floor(cartItem.payCount / 3);
-    }
-  });
 };
