4e70ed70b3a30a2719da56b3cbbb7d3f595f6d66
dale <18717394542@163.com>
2014-12-12 22:51:30 +0800

    abstract getTagCount() in scanner.js

--- pos_v2_3rdlib/src/model/scanner.js
+++ pos_v2_3rdlib/src/model/scanner.js
@@ -1,15 +1,19 @@
 function Scanner(){
 }
 
-Scanner.prototype.scan = function(tag) {
-  var splitedArray = tag.split('-');
-  var barcode = splitedArray[0];
+Scanner.prototype.getTagCount = function(tagCount) {
   var count = 1;
-
-  var hasCount = !!splitedArray[1];
+  var hasCount = !!tagCount;
   if(hasCount) {
-    count = parseFloat(splitedArray[1]);
+    count = parseFloat(tagCount);
   }
+  return count;
+};
+
+Scanner.prototype.scan = function(tag) {
+  var splitArray = tag.split('-');
+  var barcode = splitArray[0];
+  var count = this.getTagCount(splitArray[1]);
 
   var item = _.find(Item.all(), { barcode : barcode });
   var cartItem = new CartItem(item, count);
