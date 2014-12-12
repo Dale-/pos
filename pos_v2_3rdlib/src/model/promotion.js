607e6fb01d050842677f64d67f87e72740127363
dale <18717394542@163.com>
2014-12-12 22:55:10 +0800

    abstract Promotion.all() in promotion.js

--- pos_v2_3rdlib/src/model/promotion.js
+++ pos_v2_3rdlib/src/model/promotion.js
@@ -3,11 +3,14 @@ function Promotion(type, barcodes) {
     this.barcodes = barcodes || [];
 }
 
+Promotion.all = function() {
+  return loadPromotions();
+};
+
 Promotion.getPromotionType = function(barcode) {
   var promotionType = '';
-  var promotions = loadPromotions();
 
-  _.forEach(promotions, function(promotion) {
+  _.forEach(Promotion.all(), function(promotion) {
       if(_.contains(promotion.barcodes, barcode)){
         promotionType = promotion.type;
       }
