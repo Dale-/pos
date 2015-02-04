3143ef71463cd32126a66002e6f464faf4cf9031
dale <18717394542@163.com>
2015-02-04 19:44:53 +0800

    add method getName in cart-item.js

--- pos_v3/src/model/cart-item.js
+++ pos_v3/src/model/cart-item.js
@@ -10,4 +10,12 @@ CartItem.prototype.getPrice = function() {
   return this.item.price;
 };
 
+CartItem.prototype.getBrand = function() {
+    return this.item.brand;
+};
+
+CartItem.prototype.getName = function() {
+    return this.item.name;
+};
+
 module.exports = CartItem;
--- pos_v3/src/model/strategy.js
+++ pos_v3/src/model/strategy.js
@@ -1,8 +1,48 @@
+var _ = require('lodash');
+var Discount = require('./promotion/discount');
+var Promotion = require('./promotion/promotion');
+
 function Strategy() {
 }
 
 Strategy.getStrategy1String = function(cartItems) {
+    var promotionInfo = '';
+    promotionInfo += Strategy.calculateBrandPromotion(cartItems);
+    var items = [];
+    _.forEach(cartItems, function(cartItem) {
+       if(!cartItem.isPromotion) {
+           items.push(cartItem);
+       }
+    });
 
+};
+
+Strategy.calculateBrandPromotion = function(cartItems) {
+    var brandPromotionInfo = '';
+    _.forEach(Promotion.brands, function(brand) {
+        var brandCartItems = [];
+        _.forEach(cartItems ,function(cartItem) {
+            if(cartItem.getBrand() === brand.name) {
+                brandCartItems.push(cartItem);
+            }
+        });
+        brandPromotionInfo += Discount.brand(brandCartItems, brand.rate, brand.name);
+    });
+    return brandPromotionInfo;
+};
+
+Strategy.calculateItemPromotion = function(cartItems) {
+    var itemPromotionInfo = '';
+    _.forEach(Promotion.items, function(item) {
+        var itemCartItems = [];
+        _.forEach(cartItems ,function(cartItem) {
+            if(cartItem.getBrand() === item.name) {
+                itemCartItems.push(cartItem);
+            }
+        });
+        itemPromotionInfo += Discount.brand(brandCartItems, brand.rate, brand.name);
+    });
+    return itemPromotionInfo;
 };
 
 module.exports = Strategy;
