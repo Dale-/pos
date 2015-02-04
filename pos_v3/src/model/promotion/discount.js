4c5e0b03cf611d2a4de14867a1ebf5a00b902b63
dale <18717394542@163.com>
2015-02-04 19:10:45 +0800

    #pos_v3# promotion.js

--- pos_v3/src/model/promotion/discount.js
+++ pos_v3/src/model/promotion/discount.js
@@ -8,6 +8,7 @@ Discount.brand = function(cartItems, rate, brand) {
     var brandSavingMoney = 0;
     var savingRate = 1 - rate;
     _.forEach(cartItems, function(cartItem) {
+        cartItem.isPromotion = true;
         brandSavingMoney += cartItem.count * cartItem.getPrice() * savingRate;
     });
     return '名称：' + brand + '品牌打折，金额：' + brandSavingMoney.toFixed(2) + '元\n';
