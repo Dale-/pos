567a509675757909cebf890ff2623084b520483e
dale <18717394542@163.com>
2015-02-04 15:01:27 +0800

    #pos_v3# add property promotionPrice and promotionInfo in cartItem

--- pos_v3/src/model/cartItem.js
+++ pos_v3/src/model/cartItem.js
@@ -1,6 +1,8 @@
 function CartItem(item, count) {
     this.item = item;
     this.count = count;
+    this.promotionPrice = 0;
+    this.promotionInfo = '';
 }
 
 module.exports = CartItem;
---
+++ pos_v3/src/model/promotion/discount.js
@@ -0,0 +1,7 @@
+function Discount() {
+
+}
+
+Discount.brand = function(cartItems, rate) {
+
+};
