c28bb5301c7190aada53a4b85b0321e645d8cc24
dale <18717394542@163.com>
2015-02-05 11:30:55 +0800

    #pos_v3# add method item in discount.js

--- pos_v3/src/model/promotion/discount.js
+++ pos_v3/src/model/promotion/discount.js
@@ -14,4 +14,10 @@ Discount.brand = function(cartItems, rate, brand) {
     return '名称：' + brand + '品牌打折，金额：' + brandSavingMoney.toFixed(2) + '元\n';
 };
 
+Discount.item = function(cartItem, rate) {
+    cartItem.isPromotion = true;
+    var itemSavingMoney = cartItem.count * cartItem.getPrice() * (1 - rate);
+    return '名称：' + cartItem.getName() + '单品打折，金额：' + itemSavingMoney.toFixed(2) + '元\n';
+};
+
 module.exports = Discount;
