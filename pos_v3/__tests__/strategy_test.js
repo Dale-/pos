a2b798cae53a5d75dc1c2d3f1e5018faadfc2349
dale <18717394542@163.com>
2015-02-05 11:42:23 +0800

    #pos_v3# put cartItems into beforeEach in strategy_test.js

--- pos_v3/__tests__/strategy_test.js
+++ pos_v3/__tests__/strategy_test.js
@@ -11,18 +11,34 @@ describe('Strategy', function() {
     var Item;
     var CartItem;
     var Strategy;
+    var cartItems;
 
     beforeEach(function() {
         Item = require('../src/model/item');
         Strategy = require('../src/model/strategy');
         CartItem = require('../src/model/cart-item');
+        cartItems = [
+                     new CartItem(Item.all()[0], 20),
+                     new CartItem(Item.all()[1], 20),
+                     new CartItem(Item.all()[7], 30),
+                     new CartItem(Item.all()[5], 12)
+        ];
     });
 
     describe('.calculateBrandPromotion', function() {
         it('should return string of brandPromotion information', function() {
-            var cartItems = [new CartItem(Item.all()[0], 20), new CartItem(Item.all()[1], 20)];
             var result = Strategy.calculateBrandPromotion(cartItems);
             expect(result).toBe('名称：可口可乐品牌打折，金额：14.00元\n');
         });
     });
+
+    //calculateItemPromotion
+
+    //describe('.calculateBrandPromotion', function() {
+    //    it('should return string of brandPromotion information', function() {
+    //        var cartItems = [new CartItem(Item.all()[0], 20), new CartItem(Item.all()[1], 20)];
+    //        var result = Strategy.calculateBrandPromotion(cartItems);
+    //        expect(result).toBe('名称：可口可乐品牌打折，金额：14.00元\n');
+    //    });
+    //});
 });
