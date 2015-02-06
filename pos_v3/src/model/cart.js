9a02acf004aea22d72917d0a10cfb60927d3825a
dale <18717394542@163.com>
2015-02-06 10:38:51 +0800

    #pos_v3# format cart.js

--- pos_v3/src/model/cart.js
+++ pos_v3/src/model/cart.js
@@ -13,20 +13,20 @@ function Cart() {
 }
 
 Cart.prototype.addCartItem = function(tag) {
-  for(var key in tag) {
-    var item = _.find(Item.all(), {barcode: key});
-    this.cartItems.push(new CartItem(item, tag[key]));
-  }
-  return this.cartItems;
+    for(var key in tag) {
+      var item = _.find(Item.all(), {barcode: key});
+      this.cartItems.push(new CartItem(item, tag[key]));
+    }
+    return this.cartItems;
 };
 
 Cart.prototype.getListInfo = function() {
-  var cartItems = this.cartItems;
-  var listText = '';
-  _.forEach(cartItems, function(cartItem) {
-    listText += cartItem.toListText();
-  });
-  return listText;
+    var cartItems = this.cartItems;
+    var listText = '';
+    _.forEach(cartItems, function(cartItem) {
+      listText += cartItem.toListText();
+    });
+    return listText;
 };
 
 Cart.prototype.getPromotionInfo = function(strategyType) {
@@ -36,13 +36,13 @@ Cart.prototype.getPromotionInfo = function(strategyType) {
 };
 
 Cart.prototype.getSavingMoney = function() {
-  var savingMoney = 0;
-  var savings = (this.promotionInfo).split('\n');
-  savings.pop();
-  _.forEach(savings, function(saving) {
-    savingMoney += parseFloat(saving.slice(saving.lastIndexOf('：') + 1, saving.indexOf('元')));
-  });
-  return savingMoney;
+    var savingMoney = 0;
+    var savings = (this.promotionInfo).split('\n');
+    savings.pop();
+    _.forEach(savings, function(saving) {
+        savingMoney += parseFloat(saving.slice(saving.lastIndexOf('：') + 1, saving.indexOf('元')));
+    });
+    return savingMoney;
 };
 
 Cart.prototype.getSavingInfo = function(strategyType) {
@@ -60,16 +60,16 @@ Cart.prototype.getTotalInfo = function(strategyType) {
 };
 
 Cart.prototype.toString = function(strategyType) {
-  return '***<没钱赚商店>购物清单***\n' + '打印时间：' +
-         moment().format('YYYY年MM月DD日 HH:mm:ss') +
-         '\n\n----------------------\n' +
-         this.getListInfo() +
-         '\n----------------------\n' + '优惠信息：\n' +
-         this.getPromotionInfo(strategyType) +
-         '\n----------------------\n' +
-         this.getTotalInfo(strategyType) +
-         this.getSavingInfo(strategyType) +
-         '**********************\n';
+    return '***<没钱赚商店>购物清单***\n' + '打印时间：' +
+           moment().format('YYYY年MM月DD日 HH:mm:ss') +
+           '\n\n----------------------\n' +
+           this.getListInfo() +
+           '\n----------------------\n' + '优惠信息：\n' +
+           this.getPromotionInfo(strategyType) +
+           '\n----------------------\n' +
+           this.getTotalInfo(strategyType) +
+           this.getSavingInfo(strategyType) +
+           '**********************\n';
 };
 
 module.exports = Cart;
