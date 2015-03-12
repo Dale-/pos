67b1539e113f68f36a6c1a5e8397d78315759785
dale <18717394542@163.com>
2015-03-12 22:37:20 +0800

    diminish property promotionMoney in discount directory

--- pos_v3_rewrite/src/model/promotion/discount/brand.js
+++ pos_v3_rewrite/src/model/promotion/discount/brand.js
@@ -4,14 +4,13 @@ var Discount = require('./discount');
 function Brand(discountRate, totalMoney, brand) {
     Discount.call(this,discountRate, totalMoney);
     this.brand = brand;
-    this.promotionMoney = 0;
 }
 
 Brand.prototype = Object.create(Discount.prototype);
 Brand.prototype.constructor = Brand;
 
 Brand.prototype.buildPromotionInfo = function() {
-    return '名称：' + brand + '品牌打折，金额：' + this.promotionMoney + '元\n';
+    return '名称：' + this.brand + '品牌打折，金额：' + this.getPromotionMoney() + '元\n';
 };
 
 module.exports = Brand;
--- pos_v3_rewrite/src/model/promotion/discount/whole-supermarket.js
+++ pos_v3_rewrite/src/model/promotion/discount/whole-supermarket.js
@@ -3,14 +3,13 @@ var Discount = require('./discount');
 
 function WholeSupermarket(discountRate, totalMoney) {
     Discount.call(this,discountRate, totalMoney);
-    this.promotionMoney = 0;
 }
 
 WholeSupermarket.prototype = Object.create(Discount.prototype);
 WholeSupermarket.prototype.constructor = WholeSupermarket;
 
 WholeSupermarket.prototype.buildPromotionInfo = function() {
-    return '名称：' + this.discountRate * 10 + '折，金额：' + this.promotionMoney + '元\n';
+    return '名称：' + this.discountRate * 10 + '折，金额：' + this.getPromotionMoney() + '元\n';
 };
 
 module.exports = WholeSupermarket;
