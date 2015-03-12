d9fb953cbe7b3ea5fd75ca6f1fdddb2d37c3eefd
dale <18717394542@163.com>
2015-03-12 17:28:23 +0800

    completely coding cart-item.js

--- pos_v3_rewrite/src/model/cart-item.js
+++ pos_v3_rewrite/src/model/cart-item.js
@@ -4,6 +4,30 @@ function CartItem(item, count) {
     this.subTotal = 0;
 }
 
-CartItem.prototype.getSubTotal = function() {
-
+CartItem.prototype.getPrice = function() {
+    return this.item.price;
 };
+
+CartItem.prototype.getBrand = function() {
+    return this.item.brand;
+};
+
+CartItem.prototype.getName = function() {
+    return this.item.name;
+};
+
+CartItem.prototype.getUnit = function() {
+    return this.item.unit;
+};
+
+CartItem.prototype.getSubTotal = function() {
+    return (this.getPrice() * this.count).toFixed(2);
+};
+
+CartItem.prototype.buildItemInfo = function() {
+    return '名称：' + this.getName() + '，数量：' + this.count + this.getUnit() + '，' +
+           '单价：' + this.getPrice().toFixed(2) + '(元)，小计：'+ this.getSubTotal() +'(元)\n';
+};
+
+
+module.exports = CartItem;
