85ac00615316286a111a73123116183230b079a6
dale <18717394542@163.com>
2015-03-12 13:49:35 +0800

    add function run in main.js

--- pos_v3_refactor/src/main.js
+++ pos_v3_refactor/src/main.js
@@ -1,9 +1,24 @@
 var Cart = require('./model/cart');
 var Cashier = require('./model/cashier');
 
+run();
+
 function printInventory(collections, strategyType) {
 
   var cart = new Cart();
   cart.cartItems = Cashier.transfer(collections);
   console.log(cart.toString(strategyType));
+}
+
+function run() {
+    var collections = [
+        { 'ITEM000000' : 20 },
+        { 'ITEM000010' : 30 },
+        { 'ITEM000001' : 30 },
+        { 'ITEM000007' : 40 },
+        { 'ITEM000003' : 8  },
+        { 'ITEM000002' : 14 }
+    ];
+    var strategyType = '4';
+    printInventory(collections, strategyType);
 }
