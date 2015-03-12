cd5cbede57c14314c4cf2cfe0a4e99e88eb4f4a1
dale <18717394542@163.com>
2015-03-12 22:35:40 +0800

    modify discount.js

--- pos_v3_rewrite/src/model/promotion/discount/item.js
+++ pos_v3_rewrite/src/model/promotion/discount/item.js
@@ -4,14 +4,13 @@ var Discount = require('./discount');
 function Item(discountRate, totalMoney, itemName) {
     Discount.call(this,discountRate, totalMoney);
     this.itemName = itemName;
-    this.promotionMoney = 0;
 }
 
 Item.prototype = Object.create(Discount.prototype);
 Item.prototype.constructor = Item;
 
 Item.prototype.buildPromotionInfo = function() {
-    return '名称：' + itemName + '单品打折，金额：' + this.promotionMoney + '元\n';
+    return '名称：' + this.itemName + '单品打折，金额：' + this.getPromotionMoney() + '元\n';
 };
 
 module.exports = Item;
--- pos_v3_rewrite/src/model/promotion/full-reduce/full-reduce.js
+++ pos_v3_rewrite/src/model/promotion/full-reduce/full-reduce.js
@@ -1,3 +1,12 @@
-function FullReduce() {
-
+function FullReduce(fullMoney, reducedMoney, totalMoney) {
+    this.fullMoney = fullMoney;
+    this.reducedMoney = reducedMoney;
+    this.totalMoney = totalMoney;
 }
+
+FullReduce.prototype.getPromotionMoney = function() {
+    this.promotionMoney = (this.totalMoney * (1 - this.discountRate)).toFixed(2);
+    return this.promotionMoney;
+};
+
+module.exports = FullReduce;
