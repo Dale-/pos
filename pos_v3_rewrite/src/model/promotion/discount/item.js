ff295f5f1fdafac765201997467b944265a1724d
dale <18717394542@163.com>
2015-03-12 22:27:25 +0800

    completely wholeSupermarket.js

--- pos_v3_rewrite/src/model/promotion/discount/item.js
+++ pos_v3_rewrite/src/model/promotion/discount/item.js
@@ -1,3 +1,17 @@
-/**
- * Created by dale on 12/03/15.
- */
+var _ = require('lodash');
+var Discount = require('./discount');
+
+function Item(discountRate, totalMoney, itemName) {
+    Discount.call(this,discountRate, totalMoney);
+    this.itemName = itemName;
+    this.promotionMoney = 0;
+}
+
+Item.prototype = Object.create(Discount.prototype);
+Item.prototype.constructor = Item;
+
+Item.prototype.buildPromotionInfo = function() {
+    return '名称：' + itemName + '单品打折，金额：' + this.promotionMoney + '元\n';
+};
+
+module.exports = Item;
