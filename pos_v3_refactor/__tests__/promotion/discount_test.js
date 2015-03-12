58728adb000c04631488fd2b55ab5904e509808e
dale <18717394542@163.com>
2015-03-12 13:43:58 +0800

    give pos_v3_refactor add __tests__

---
+++ pos_v3_refactor/__tests__/promotion/discount_test.js
@@ -0,0 +1,33 @@
+jest.dontMock('lodash');
+jest.dontMock('../../src/model/item');
+jest.dontMock('../../src/model/cart-item');
+jest.dontMock('../../src/model/promotion/discount');
+
+describe('Discount', function() {
+
+    var Item;
+    var CartItem;
+    var Discount;
+
+    beforeEach(function() {
+        Item = require('../../src/model/item');
+        CartItem = require('../../src/model/cart-item');
+        Discount = require('../../src/model/promotion/discount');
+    });
+
+    describe('.brand', function() {
+        it('should return string of discount information', function() {
+            var cartItems = [new CartItem(Item.all()[0], 20), new CartItem(Item.all()[1], 20)];
+            var result = Discount.brand(cartItems, 0.9, '可口可乐');
+            expect(result).toBe('名称：可口可乐品牌打折，金额：14.00元\n');
+        });
+    });
+
+    describe('.item', function() {
+        it('should return string of discount information', function() {
+            var cartItem = new CartItem(Item.all()[0], 20);
+            var result = Discount.item(cartItem, 0.95);
+            expect(result).toBe('名称：可口可乐350ml单品打折，金额：3.00元\n');
+        });
+    });
+});
